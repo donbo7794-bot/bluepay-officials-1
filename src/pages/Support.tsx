6dcff8125e7c485c1a09119b644037101b791fc9
donb01326-hue <donb01326@gmail.com>
2026-01-04 11:55:34 +0100

    Update live chat link in Support component

--- src/pages/Support.tsx
+++ src/pages/Support.tsx
@@ -8,7 +8,7 @@ const Support = () => {
   const navigate = useNavigate();
 
   const handleLiveChatClick = () => {
-    window.open('https://t.me/Redpayagent1', '_blank');
+    window.open('https://t.me/Matthewxx8230', '_blank');
   };
 
   return (
